8c1ffc280f6f9b264834bdcc9dc0cd658fc45bde
Guido564 <75608723+Guido564@users.noreply.github.com>
2022-01-26 22:48:52 -0300

    Add files via upload

---
+++ saludoBienvenida/index.js
@@ -0,0 +1,7 @@
+let welcomeEl = document.getElementById("welcome-el");
+let nombre = "Guido";
+let saludo = "Bienvenido otra vez"
+
+welcomeEl.innerText =  saludo + " " + nombre + "!";
+
+welcomeEl.innerText += " 🥰" 
